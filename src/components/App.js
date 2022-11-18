c601452a94cb6cc0928dd9e2cb7431db21f61cd0
tinld <19522348@gm.uit.edu.vn>
2022-11-18 09:21:07 +0700

    update register page

--- src/components/App.js
+++ src/components/App.js
@@ -2,7 +2,7 @@ import {Route, Routes} from "react-router-dom"
 import Login from "../pages/Login";
 import About from "../pages/About";
 import NewFeed from "../pages/NewFeed";
-import RegisterForm from "./Register";
+import RegisterForm from "./RegisterForm";
 import NavBar from "./Navbar";
 import ForumBreed from "../pages/ForumBreed";
 import Forum from "../pages/Forum"
@@ -10,6 +10,7 @@ import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
 import { persistStore } from "redux-persist";
 import store from "./store/store";
+import Register from "../pages/Register";
 function App() {
 
   const persistor = persistStore(store);
@@ -25,7 +26,7 @@ function App() {
             <Route path="about" element={<About />} />
           </Route>
           <Route path="login" element = {<Login />} />
-          <Route path="register" element={<RegisterForm />} />
+          <Route path="register" element={<Register />} />
         </Routes>
       </PersistGate>
     </Provider>
