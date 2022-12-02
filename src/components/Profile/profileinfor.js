848713f70512ace30fc8746f49cf3542e7d0a459
tinld <19522348@gm.uit.edu.vn>
2022-12-02 09:42:52 +0700

    user profile

--- src/components/Profile/profileinfor.js
+++ src/components/Profile/profileinfor.js
@@ -4,51 +4,57 @@ import Row from 'react-bootstrap/Row';
 import "../../styles/profile.css"
 import Card from 'react-bootstrap/Card';
 import { useSelector } from 'react-redux';
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
+import { GetUserId } from '../features/users/allUserSlice';
 
 function Profileinfor() {
     const user = useSelector((state) => state.user);
     const Navigate = useNavigate()
-    
-  return (
-    <>
+    const {userId} = useParams();
 
-        {
-            user.userInfo? (
-                <Container>
-                    <Row>
-                        <div className='content-news-first'>
-                            <h3 className='title'>THÔNG TIN TÀI KHOẢN</h3>
-                        </div>
-                    </Row>
-                    <Card className='card1'>
-                            <Card.Img className="card-news-image" variant="top" src={user.userInfo.BackgroundImg} />
-                            <Card.Img className='userpic' src={user.userInfo.Avatar || "https://cdn-icons-png.flaticon.com/512/44/44948.png"}/>
-                            <Card.Body>
-                            <Card.Title className='un'>{user.userInfo.UserName}</Card.Title>
-                            </Card.Body>
-                    </Card>
-                    <Card className='card2'>
-                            <Card.Body>
-                            
-                            <Card.Text>
-                                <strong>Email:</strong> {user.userInfo.Email}
-                            </Card.Text>
-                            <Card.Text>
-                                <strong>Ngày tạo:</strong> {user.userInfo.RegisterDate}
-                            </Card.Text>
-                            <Card.Text>
-                                <strong>Danh hiệu:</strong> 
-                            </Card.Text>
-                            </Card.Body>
-                    </Card>
-                </Container>
-            ) : (
-                Navigate('/')
-            )
-        }
-    </>
-  )
-}
+    const userProfile = useSelector((state) => GetUserId(state, userId));
+    const CreatedDate = new Date(userProfile[0].CreatedDate).toUTCString();
+
+
+    return (
+        <>
+
+            {
+                userProfile? (
+                    <Container>
+                        <Row>
+                            <div className='content-news-first'>
+                                <h3 className='title'>THÔNG TIN TÀI KHOẢN</h3>
+                            </div>
+                        </Row>
+                        <Card className='card1'>
+                                <Card.Img className="card-news-image" variant="top" src={userProfile[0].BackgroundImg} />
+                                <Card.Img className='userpic' src={userProfile[0].Avatar || "https://cdn-icons-png.flaticon.com/512/44/44948.png"}/>
+                                <Card.Body>
+                                <Card.Title className='un'>{userProfile[0].UserName}</Card.Title>
+                                </Card.Body>
+                        </Card>
+                        <Card className='card2'>
+                                <Card.Body>
+                                
+                                <Card.Text>
+                                    <strong>Email:</strong> {userProfile[0].Email}
+                                </Card.Text>
+                                <Card.Text>
+                                    <strong>Ngày tạo:</strong> {CreatedDate}
+                                </Card.Text>
+                                <Card.Text>
+                                    <strong>Danh hiệu:</strong> 
+                                </Card.Text>
+                                </Card.Body>
+                        </Card>
+                    </Container>
+                ) : (
+                    Navigate('/')
+                )
+            }
+        </>
+    )
+    }
 
 export default Profileinfor;
