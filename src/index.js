721dfc92061a23bd7d5949adba4a7e20748181c5
ThanhOwO <91793770+ThanhOwO@users.noreply.github.com>
2022-12-11 15:06:56 +0700

    merge final code from main

--- src/index.js
+++ src/index.js
@@ -8,6 +8,7 @@ import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from './components/store/store';
 import NotificationProvider from 'use-toast-notification';
+// LeSyThanh was here
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
